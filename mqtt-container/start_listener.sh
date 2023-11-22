bfbbaf93d19900e5c21129d3057b317faeb6ad13
Koen Braham <koen.braham@alten.nl>
2023-11-22 17:07:11 +0100

    Update IP of listener script

--- mqtt-container/start_listener.sh
+++ mqtt-container/start_listener.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-podman run -it --entrypoint /bin/sh eclipse-mosquitto -c "/usr/bin/mosquitto_sub -h 192.168.1.189 -v -t \"#\""
+podman run -it --entrypoint /bin/sh eclipse-mosquitto -c "/usr/bin/mosquitto_sub -h 192.168.1.190 -v -t \"#\""
